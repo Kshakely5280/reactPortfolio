1685a38eaa72233cc5b2ea5226e7c0af1d221d6e
Kyle Shakely <kyleshakely@yahoo.com>
2023-05-21 16:41:54 -0600

    got app started and added structure and footer to test connection

---
+++ portfolio/src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+      <footer>
+        <div className="social-icons">
+          <a href="https://github.com/kshakely5280">
+            <FaGithub />
+          </a>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
