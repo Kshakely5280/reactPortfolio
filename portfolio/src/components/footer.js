166460624ed8f5aaa6b806324d5d1754f9004545
Kyle Shakely <kyleshakely@yahoo.com>
2023-05-22 17:31:52 -0600

    added some extra icons and css to the footer

--- portfolio/src/components/footer.js
+++ portfolio/src/components/footer.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="social-icons">
-        <a href="https://github.com/kshakely5280">
+        <a href="https://github.com/your-github-profile-url" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
+        <a href="https://www.linkedin.com/your-linkedin-profile-url" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.facebook.com/your-facebook-profile-url" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
       </div>
-      <div>GoBabyGo 2023</div>
+      <div className="footer-text">GoBabyGo 2023</div>
     </footer>
   );
 };
