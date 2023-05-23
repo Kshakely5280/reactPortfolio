19328ae66bb5bdb84ecf9e979ba2f3222dd8adee
Kyle Shakely <kyleshakely@yahoo.com>
2023-05-22 18:04:56 -0600

    cleaned up the contact page, added err message if email is invalid, added placeholders and confirmation of successful messsage

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -4,15 +4,29 @@ const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [showError, setShowError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-// console logging for testing purposes
+
+    if (!validateEmail(email)) {
+      setShowError(true);
+      return;
+    }
+
+    // Process the form submission
     console.log({ name, email, message });
+
+    // Clear the form inputs
+    setName('');
+    setEmail('');
+    setMessage('');
+    setShowError(false);
+    setSubmitted(true);
   };
 
   const validateEmail = (email) => {
-    // Email validation regex pattern
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
@@ -23,6 +37,7 @@ const Contact = () => {
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
+    setShowError(false);
   };
 
   const handleMessageChange = (e) => {
@@ -30,24 +45,28 @@ const Contact = () => {
   };
 
   return (
-    <div>
+    <div className="contact-form">
       <h2>Contact Me</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" value={name} onChange={handleNameChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" value={email} onChange={handleEmailChange} />
-          {!validateEmail(email) && <span>Please enter a valid email address.</span>}
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" value={message} onChange={handleMessageChange} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      {submitted ? (
+        <p className="thank-you-message">Thank you! I will be in contact with you soon.</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" value={name} onChange={handleNameChange} placeholder="Enter your name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" value={email} onChange={handleEmailChange} placeholder="Enter your email address" />
+            {showError && <span className="error-message">Please enter a valid email address.</span>}
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" value={message} onChange={handleMessageChange} placeholder="Enter your message"></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
