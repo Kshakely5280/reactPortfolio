5614cd7c8b290d5357a5b65d6580de14fcacdfc4
Kyle Shakely <kyleshakely@yahoo.com>
2023-05-23 16:54:34 -0600

    beefed up about me section

--- portfolio/src/components/aboutMe.js
+++ portfolio/src/components/aboutMe.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-
 const AboutMe = () => {
   return (
-    <div>
-      WHY CAN YOU SEE THIS
+    <div className="aboutContainer">
+      <div className="picDiv">
+        <img src="./headshot.jpg" alt="Headshot" />
+      </div>
+      <div className="textDiv">
+        <h2>About Me</h2>
+        <p>
+          Hi there! My name is Kyle Shakely, and I'm an aspiring programmer. Welcome to my coding portfolio website!
+        </p>
+        <p>
+          Apart from coding, I have a deep passion for cars, hiking, and camping. Exploring the great outdoors, breathing in fresh mountain air, and challenging myself on scenic trails are some of the things that truly invigorate me. Additionally, I love diving into the world of automobiles, keeping up with the latest industry trends, and learning about cutting-edge automotive technologies.
+        </p>
+        <p>
+          I'm fascinated by the art of coding and the limitless possibilities it offers. With a strong desire to create innovative solutions and bring ideas to life, I am constantly honing my skills in various programming languages and technologies. Through my projects and experiences, I aim to showcase my problem-solving abilities and passion for clean, efficient code.
+        </p>
+        <p>
+          I'm excited about the journey ahead as I continue to learn and grow as a programmer. Feel free to explore my portfolio and get in touch with me if you'd like to collaborate or discuss any opportunities. Let's create something incredible together!
+        </p>
+      </div>
     </div>
   );
 };
