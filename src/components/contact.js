63df6da95491d1048f5576bdf2758b358d881742
Kyle Shakely <kyleshakely@yahoo.com>
2023-07-01 12:45:02 -0600

    using formspree to route contact messages to email. needs redesign

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,94 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [showError, setShowError] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
+  const [state, handleSubmit] = useForm("xyyqgoag");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!validateEmail(email)) {
-      setShowError(true);
-      return;
+  function ContactForm() {
+    if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
     }
-
-    setName("");
-    setEmail("");
-    setMessage("");
-    setShowError(false);
-    setSubmitted(true);
-  };
-
-  const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setShowError(false);
-  };
-
-  const handleMessageChange = (e) => {
-    setMessage(e.target.value);
-  };
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="text" name="name" />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" />
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
+        </div>
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
+    );
+  }
 
   return (
     <div className="contact-form">
       <h2>Contact Me</h2>
-      {submitted ? (
-        <p className="thank-you-message">
-          Thank you! I will be in contact with you soon.
-          <br />
-          If you need to contact me sooner, reach me at 303-802-6044 or
-          Kyleshakely@yahoo.com
-        </p>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={handleNameChange}
-              placeholder="Enter your name (Required)"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="Enter your email address (Required)"
-            />
-            {showError && (
-              <span className="error-message">
-                Please enter a valid email address.
-              </span>
-            )}
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={handleMessageChange}
-              placeholder="Enter your message (Required)"
-            ></textarea>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      )}
+      <ContactForm />
     </div>
   );
 };
